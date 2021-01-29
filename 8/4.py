52ab51de619b6b700dc1ae044fb18b4261965393
JUNGEEYOU <junge2u@naver.com>
2021-01-29 22:56:32 +0900

    1로 만들기

--- 8/4.py
+++ 8/4.py
@@ -1,4 +1,22 @@
 """
     1로 만들기
-    
+    https://www.acmicpc.net/problem/1463 비슷한 문제
 """
+import sys
+
+
+n = int(sys.stdin.readline().split()[0])
+d = [0] * (n + 1)
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i//5] + 1)
+    elif i % 3 == 0:
+        d[i] = min(d[i], d[i//5] + 1)
+    elif i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+
+
+print(d[n])
+
