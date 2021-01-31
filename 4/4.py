b5a03a1309053dca8a450686b1c2e78153d1eb2c
JUNGEEYOU <junge2u@naver.com>
2021-01-31 20:35:04 +0900

    수정

--- 4/4.py
+++ 4/4.py
@@ -6,3 +6,22 @@
  - 리턴
 
 """
+import sys
+
+n, m = map(int, sys.stdin.readline().split())    # 세로, 가로
+x, y, direction = map(int, sys.stdin.readline().split()) # (x, y) d(바라보는 방향)
+
+# 북, 동, 남, 서
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction= 3
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
--- 9/2.py
+++ 9/2.py
@@ -1,27 +1,44 @@
+"""
+6 11
+1
+1 2 2
+1 3 5
+1 4 1
+2 3 3
+2 4 2
+3 2 3
+3 6 5
+4 3 3
+4 5 1
+5 3 1
+5 6 2
+"""
 import heapq
 import sys
 
 INF = int(1e9)
 
-n, m = map(int, sys.stdin.readline().split())   # 노드 수, 간선 수
+n, m = map(int, sys.stdin.readline().split())  # 노드 수, 간선 수
 start = int(sys.stdin.readline())
 
 graph = [[] for i in range(n + 1)]
 distance = [INF] * (n + 1)
 
 for _ in range(m):
-    a, b, c = map(int, sys.stdin.readline().split())   # a => b 비용 c
+    a, b, c = map(int, sys.stdin.readline().split())  # a => b 비용 c
     graph[a].append((b, c))
+
 
 def dijkstra(start):
     q = []
-    heapq.heappush(q, (0, start))   # 시작 노드 거리를 0으로 설정
+    heapq.heappush(q, (0, start))  # 1. 시작 노드 거리를 0으로 설정
     distance[start] = 0
     while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:   # 처리된적 있으면 무시
+        dist, now = heapq.heappop(q)  # 2. 방문하지 않은 노드 중 짧은 노드 선택
+        if distance[now] < dist:  # 처리된적 있으면 무시
             continue
-        for i in graph[now]:   # 인접 노드 확인
+        # 3. 현재 선택된 노드를 거쳐가는 비용을 계산하여 최단 거리 테이블 갱신
+        for i in graph[now]:  # 인접 노드 확인
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
