b88a83ee02c91e9e75c8ba5c27b85145d2042f63
JUNGEEYOU <junge2u@naver.com>
2021-02-11 13:40:30 +0900

    구현 문제

--- 12/1.py
+++ 12/1.py
@@ -1,22 +1,7 @@
-"""
-  럭키 스트레이트
-    https://www.acmicpc.net/problem/18406
-"""
 import sys
 
-
-def sum_list(n_list):
-    result = 0
-    for n in n_list:
-        result += int(n)
-    return result
-
-
 n = str(sys.stdin.readline().split()[0])
-
-n_1 = n[:(len(n) // 2)]
-n_2 = n[(len(n) // 2):]
-if sum_list(n_1) == sum_list(n_2):
-    print("LUCKY")
-else:
-    print("READY")
+len_n = len(n)
+sum_left = sum(map(int, n[0:(len_n//2)]))
+sum_right = sum(map(int, n[(len_n//2):len_n]))
+print("LUCKY" if sum_left == sum_right else "READY")
--- 12/2.py
+++ 12/2.py
@@ -3,17 +3,15 @@
   K1KA5CB7
 """
 import sys
-s = sys.stdin.readline().split()[0]
-al_s = ''
-num_s = 0
-for c in s:
-    if c.isalpha():
-        al_s += c
-    else:
-        num_s += int(c)
-al_s_sort = ''.join(sorted(al_s))
 
-if num_s:
-    print(al_s_sort + str(num_s))
-else:
-    print(al_s_sort)
+n = str(sys.stdin.readline().split()[0])
+alpha = ''
+num = 0
+for c in n:
+    if c.isalpha():
+        alpha += c
+    else:
+        num += int(c)
+
+alpha = ''.join(sorted(alpha))
+print(alpha + str(num))
